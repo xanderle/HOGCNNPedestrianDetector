e6987aa9cc3f6343caf587592255f1af8716f138
xanderle <x.marks.the.spot.xd@gmail.com>
2017-06-30 16:16:18 +0800

    Navigating through directories

--- code3.2.1/readCaltech.m
+++ code3.2.1/readCaltech.m
@@ -1,38 +1,49 @@
-names = dir('data-USA/annotations/set00/V000/*.txt') ;
-annotations = fullfile('data-USA','annotations','set00','V000',{names.name});
+setDir = dir('data-USA/images/');
+setDir(1:3) = [];
+setFull = fullfile('data-USA','images',{setDir.name});
 
-names = dir('data-USA/images/set00/V000/*.jpg');
-images = fullfile('data-USA','images','set00','V000',{names.name});
-
+annotFull = fullfile('data-USA','annotations',{setDir.name});
 counter = 0;
 
-for i=1:numel(annotations)
-    fileID = fopen(annotations{i},'r');
-    fgetl(fileID);
-    line = fgetl(fileID);
-    im = imread(images{i});
-    imshow(im);
-    while ischar(line)
-        a = textscan(line,'%s');
-        if a{1}{6} == 0
-            continue
+for i=1:5
+    video = dir(imgFull{i})
+    video(1:2) = [];
+    videoFull = fullfile(setFull{i},{video.name});
+    annotVid = fullfile(annotFull{i},{video.name});
+    for j=1:numel(video)
+        names = dir([annotVid{i} '/*.txt']);
+        annotations = fullfile(annotVid{i},{names.name});
+        names = dir([videoFull{i} '/*.jpg']);
+        images = fullfile(videoFull{i},{names.name});
+        for k=1:numel(annotations)
+            fileID = fopen(annotations{k},'r');
+            fgetl(fileID);
+            line = fgetl(fileID);
+            im = imread(images{k});
+            %imshow(im);
+            while ischar(line)
+                a = textscan(line,'%s');
+                if a{1}{6} == 0
+                    continue
+                end
+                if strcmp(a{1}{1},'person')
+                    bbox = [str2num(a{1}{2}),str2num(a{1}{3}),str2num(a{1}{4}),str2num(a{1}{5})];
+                    %rectangle('Position',bbox,'EdgeColor','g');
+                    width = bbox(3);
+                    height = bbox(4);
+                    ratio = 0.3; %width/height;
+                    newWidth = ratio*height;
+                    newX = bbox(1)+(width-newWidth)/2;
+                    newBBox = [newX,bbox(2),newWidth,bbox(4)];
+                    im2 = imcrop(im,newBBox);
+                    savePositiveDir = strcat('myPositives/',num2str(counter),'.png');
+                    imwrite(im2,savePositiveDir);
+                    counter = counter + 1;
+                end
+                line = fgetl(fileID);
+            end
+            
+            fclose(fileID);
         end
-        if strcmp(a{1}{1},'person')
-                bbox = [str2num(a{1}{2}),str2num(a{1}{3}),str2num(a{1}{4}),str2num(a{1}{5})]
-                rectangle('Position',bbox,'EdgeColor','g');
-                width = bbox(3);
-                height = bbox(4);
-                ratio = 0.3; %width/height;
-                newWidth = ratio*height;
-                newX = bbox(1)+(width-newWidth)/2;
-                newBBox = [newX,bbox(2),newWidth,bbox(4)];
-                im2 = imcrop(im,newBBox);
-                savePositiveDir = strcat('myPositives/',num2str(counter),'.png');
-                imwrite(im2,savePositiveDir);
-                counter = counter + 1;
-        end
-        line = fgetl(fileID);
     end
-    pause(0.5)
-    fclose(fileID);
 end
